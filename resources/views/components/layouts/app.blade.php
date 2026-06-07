f7963449c6f789c26a2c136219c55127d09332ba
Devoldere Benny <devolderebenny@gmail.com>
2026-06-07 17:00:26 +0200

    registratieformulier

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Anime Webshop') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans antialiased text-gray-900 bg-gray-100">
+
+<main>
+    {{ $slot }}
+</main>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Volt::route('/register', 'pages.auth.register')
+    ->middleware('guest')
+    ->name('register');
